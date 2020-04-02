1d6860119b88b3840a72244b950db9149e8c9680
Syahrin Seth <syahrin_seth@yahoo.com>
2020-04-02 13:35:11 +0800

    Update: api setup

--- plumber.R
+++ plumber.R
@@ -1,108 +1,78 @@
 
-# script name:
-# plumber.R
 
-# set API title and description to show up in http://localhost:8000/__swagger__/
 
-#' @apiTitle Machine Learning Probability Calculator for ACS Mortality 
-#' @apiDescription This API takes as patient data and returns a probability of survival and non-survival
-#' with values between 0 and 1.
 
-# can combine all parameters in one plumber.R file
-# but must have different parameter names for different models (.rds files)
 
-# load model
-# this path would have to be adapted if you would deploy this
-model_rfinhosp <- readRDS("fmodelinhosp.rds")
-model_rf30d <- readRDS("fmodel30d.rds")
-model_rf1y <- readRDS("fmodel1y.rds")
 
-#' Log system time, request method and HTTP user agent of the incoming request
-#' @filter logger
-function(req){
-  cat("System time:", as.character(Sys.time()), "\n",
-      "Request method:", req$REQUEST_METHOD, req$PATH_INFO, "\n",
-      "HTTP user agent:", req$HTTP_USER_AGENT, "@", req$REMOTE_ADDR, "\n")
-  plumber::forward()
-}
-
-# core function follows below:
-# define parameters with type and description
-# name endpoint
-# return output as html/text
-# specify 200 (okay) return
-
-#' predict survival probability of ACS patients with Random Forest model
-#' @param age:numeric years
-#' @param heartrate:numeric Norm: 
-#' @param bpsys:numeric Norm: 
-#' @param killipclass:numeric Norm:
-#' @param tc:numeric Norm:
-#' @param fbg:numeric Norm:
-#' 
-#' @param bpdias:numeric Norm:
-#' @param cardiacarrest:numeric Norm:
-#' @param hdlc:numeric Norm:
-#' @param ldlc:numeric Norm:
-#' @param tg:numeric Norm:
-#' @param bb:numeric Norm:
-#' @param lipidla:numeric Norm:
-#' @param acei:numeric Norm:
-#' 
-#' 
-
-#' @get /predict
-#' @html
-
-#' @response 200 Returns the probability prediction of survival and non-survival of patients
-#' Random Forest model; values are between 0 and 1
-calculate_prediction <- function(age, heartrate, bpsys, killipclass, tc, fbg, bpdias, cardiacarrest, hdlc, ldlc, tg, bb, lipidla, acei) {
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+calculate_prediction <- function( bonePart, angulation, fractureDiameter, age ) {
   
-  #make dataframe for IN HOSPITAL
-  input_datainhosp <<- data.frame(age, heartrate, bpsys, bpdias, killipclass, cardiacarrest, tc, hdlc, ldlc, tg, fbg, bb, stringsAsFactors = FALSE)
+  library(caret)
+  data <- read.csv ( file="/Users/syahrinseth/documents/projects/UpperLimb/FYP_UpperLimb/SelRF.csv")
+  #data <- read.delim("SelRF.txt", sep = '\t', header = TRUE)
+  set.seed(240)
+  intrain <- createDataPartition(data$HealTimeWeeks, p= 0.7, list = FALSE)
+  training <- data[intrain,]
+  testing <- data[-intrain,]
+  capture.output(testing,file='testingRandomSampling.txt', append=TRUE)
   
+  x <- training[,-5]
+  y <- training[,5]
+  
+  CV_Folds <- createMultiFolds(y, k = 3, times = 3)
+  
+  R_model <- train(data.frame(x),y, method="svmRadial",tuneLength=5,
+                   trControl=trainControl(method='repeatedCV',index=CV_Folds))
+  R_model
+  
+  predresult <- predict(R_model, testing[,-5])
+  
+  predresult
+  
+  predicted <- ((predresult*10)+2)
+  Actual <- (((testing$HealTimeWeeks)*10)+2)
+  
+  plot(Actual,predicted)
+  
+  rmse <- sqrt(sum((Actual - predicted)^2)/nrow(testing))
+  
+  rmse
+  
+  saveRDS(R_model, "./fmodel1y.rds")
+  ###new testing
+  
+  
+  
+  
+  # start a different file plumber.r
+  # load saved machine learning prediction model
+  
+  model_rfinhosp <- readRDS("./fmodel1y.rds")
+
+  
+  
+  #create a data frame capture from php form
+ 
+  x <- data.frame(sapply(bonePart, as.integer), sapply(angulation, as.integer), sapply(fractureDiameter, as.integer), sapply(age, as.integer), stringsAsFactors = FALSE)
+
   #make sure they are numeric
-  input_datainhosp <<- as.data.frame(t(sapply(input_datainhosp, as.integer)))
-  
-  colnames(input_datainhosp) <- c("age", "heartrate", "bpsys", "bpdias", "killipclass", "cardiacarrest", "tc", "hdlc", "ldlc", "tg", "fbg", "bb")
-
-  pred_rfinhosp <<- predict(model_rfinhosp, input_datainhosp, type="prob")
-  result_inhosp <<- data.frame(pred_rfinhosp)
-  colnames(result_inhosp) <- c("survival", "Nonsurvival")
-  #paste("", as.character(result$survival), sep = "")
-  
-  
-  
-  #make dataframe for 30DAYS
-  input_data30 <<- data.frame(age, heartrate, bpsys, bpdias, killipclass, tc, tg, fbg, lipidla, stringsAsFactors = FALSE)
-  
-  #make sure they are numeric
-  input_data30 <<- as.data.frame(t(sapply(input_data30, as.integer)))
-  
-  colnames(input_data30) <- c("age", "heartrate", "bpsys", "bpdias", "killipclass", "tc", "tg", "fbg", "lipidla")
-  
-  pred_rf30 <<- predict(model_rf30d, input_data30, type="prob")
-  result30 <<- data.frame(pred_rf30)
-  colnames(result30) <- c("survival", "Nonsurvival")
-  
-  
-  
-  
-  #make dataframe for 30DAYS
-  input_data1y <<- data.frame(age, heartrate, bpsys, killipclass, tc, fbg, acei, lipidla, stringsAsFactors = FALSE)
-  
-  #make sure they are numeric
-  input_data1y <<- as.data.frame(t(sapply(input_data1y, as.integer)))
-  
-  colnames(input_data1y) <- c("age", "heartrate", "bpsys", "killipclass", "tc", "fbg", "acei", "lipidla")
-  
-  pred_rf1y <<- predict(model_rf1y, input_data1y, type="prob")
-  result1y <<- data.frame(pred_rf1y)
-  colnames(result1y) <- c("survival", "Nonsurvival")
-  
-
-  paste(as.character(result_inhosp$survival), as.character(result30$survival), as.character(result1y$survival), sep = ",")
-  
-}
+  x <<- as.data.frame(t(sapply(x, as.integer)))
+ 
+  colnames(x) <- c("bonePart", "angulation", "fractureDiameter", "age")
+ 
+#make prediction using the saved rds file
+  pred_weeks <<- predict(model_rfinhosp, x)
 
 
+#put results in data frame
+  resultfracture <<- data.frame(pred_weeks)
+
+
+  denom_resultfracture <- ((resultfracture*10)+2)
+
+  colnames(denom_resultfracture) <- c("healing_weeks")
+ 
+  paste(as.character(denom_resultfracture$healing_weeks), sep = ",")
+  }
--- server.r
+++ server.r
@@ -1,3 +1,3 @@
 library(plumber)
-r <- plumb("plumbertest.R")
+r <- plumb(file="/Users/syahrinseth/documents/projects/UpperLimb/FYP_UpperLimb/plumber.R")
 r$run(port = 8000)
